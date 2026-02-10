a8875a0340b3a8bf7cb11525e5f57f472d30934a
altenergy250155-ux <altenergy.250155@gmail.com>
2026-02-10 11:14:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,12 +44,10 @@ SLACK_PROFILE_SET_URL = 'https://slack.com/api/users.profile.set'
 def get_hrmos_token():
     """HRMOS APIのトークンを取得"""
     try:
-        # Secret Keyをユーザー名として、パスワードは空でBasic認証
-        auth_string = base64.b64encode(f"{HRMOS_API_SECRET}:".encode()).decode()
         response = requests.get(
             f"{HRMOS_API_BASE}/authentication/token",
             headers={
-                'Authorization': f'Basic {auth_string}',
+                'Authorization': f'Basic {HRMOS_API_SECRET}',
                 'Content-Type': 'application/json'
             }
         )
@@ -548,13 +546,12 @@ def debug():
     try:
         if HRMOS_API_SECRET:
             import base64
-            auth_string = base64.b64encode(f"{HRMOS_API_SECRET}:".encode()).decode()
-            debug_info['auth_string_preview'] = auth_string[:20] + '...'
+            debug_info['auth_header_preview'] = f"Basic {HRMOS_API_SECRET[:20]}..."
             
             response = requests.get(
                 f"{HRMOS_API_BASE}/authentication/token",
                 headers={
-                    'Authorization': f'Basic {auth_string}',
+                    'Authorization': f'Basic {HRMOS_API_SECRET}',
                     'Content-Type': 'application/json'
                 }
             )
